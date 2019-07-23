c2a2141ff39c26545d7af85446681a8b7a11bd56
Arpit Satyal <arpited7@gmail.com>
2019-07-23 18:37:51 +0545

    new commit @ 2019-07-23T18:37:51+05:45

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// let moment = require('moment')
-// let random = require('random')
-// let simpleGit = require('simple-git')
-// let jsonFile = require('jsonfile')
-// let FILE_PATH = './data.json'
-// let THIS_FILE = './index.js'
+let moment = require('moment')
+let random = require('random')
+let simpleGit = require('simple-git')
+let jsonFile = require('jsonfile')
+let FILE_PATH = './data.json'
+let THIS_FILE = './index.js'
 
-// function makeCommit(n) {
-//     if(n === 0) return simpleGit().push()
-//             let x = random.int(0, 54)
-//             let y = random.int(0, 6)
+function makeCommit(n) {
+    if(n === 0) return simpleGit().push()
+            let x = random.int(0, 54)
+            let y = random.int(0, 6)
 
-//             let DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
+            let DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format()
 
-//             let data = { data: DATE }
-//             console.log(DATE)
+            let data = { data: DATE }
+            console.log(DATE)
 
-//             jsonFile.writeFile(FILE_PATH, data, (err, done) => {
-//                 if (err) console.log(err)
-//                 simpleGit().add([FILE_PATH, THIS_FILE]).commit(`new commit @ ${DATE}`, { '--date': DATE }, makeCommit.bind(this, --n))
-//             })
-// }
-// makeCommit(100)
+            jsonFile.writeFile(FILE_PATH, data, (err, done) => {
+                if (err) console.log(err)
+                simpleGit().add([FILE_PATH, THIS_FILE]).commit(`new commit @ ${DATE}`, { '--date': DATE }, makeCommit.bind(this, --n))
+            })
+}
+makeCommit(100)
 
 
